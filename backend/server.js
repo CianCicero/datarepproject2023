eb4f993c5eb88df6a296d138dc874d44998f6380
CianCicero <cianjenny@gmail.com>
2023-12-22 13:11:37 +0000

    fix

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 4000;
 
-
+//
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
