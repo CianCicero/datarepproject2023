63498c6ffe52efa0d2b299754b3b22de5a8b3b2b
CianCicero <cianjenny@gmail.com>
2023-12-22 03:53:00 +0000

    fixed error

--- src/components/deletesong.js
+++ src/components/deletesong.js
@@ -1,31 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 const DeleteSong = () => {
     const navigate = useNavigate();
     const { id } = useParams();
     const [songName, setSong] = useState('');
-    const [artistName, setArtist] = useState('');
-    const [genre, setGenre] = useState('');
-    const [rating, setRating] = useState('');
-    const [review, setReview] = useState('');
-    const [albumCover, setCover] = useState('');
+    // ... (other state variables)
 
     useEffect(() => {
         axios.get(`http://localhost:4000/songs/${id}`)
-        .then((res) => {
-            setSong(res.data.songName);
-            setArtist(res.data.artistName);
-            setGenre(res.data.genre);
-            setRating(res.data.rating);
-            setReview(res.data.review);
-            setCover(res.data.albumCover);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+            .then((res) => {
+                setSong(res.data.songName);
+                // ... (set other state variables)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }, [id]);
 
     const handleSubmit = (e) => {
@@ -33,34 +25,32 @@ const DeleteSong = () => {
 
         const song = {
             songName,
-            artistName,
-            genre,
-            rating,
-            review,
-            albumCover
+            // ... (other state variables)
         }
 
         axios.delete(`http://localhost:4000/songs/delete/${id}`, song)
-        .then((res) => {
-            console.log(res.data);
-            navigate("/");
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+            .then((res) => {
+                console.log(res.data);
+                navigate("/");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
     return (
         <div>
             <h1>Delete Song</h1>
 
-
-                <Button variant="primary" type="submit" className="btn btn-danger">
+            <Form onSubmit={handleSubmit}>
+                {/* Add Form controls for other input fields if needed */}
+                {/* For demonstration, I'm just using the Button here */}
+                <Button variant="primary" type="submit">
                     Delete Song
                 </Button>
+            </Form>
+        </div>
+    );
+}
 
-       </div>
-        
-    )
-    }
-    export default DeleteSong;
+export default DeleteSong;
